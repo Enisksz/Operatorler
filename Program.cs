52bf1e059463d4b8b751938ea01ed00e0b9b8595
Enisksz <71214539+Enisksz@users.noreply.github.com>
2022-03-08 14:27:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+
+            y += 2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            bool ısSuccess=true;
+            bool ısCompleted = false;
+
+            if (ısSuccess && ısCompleted)
+                Console.WriteLine("Perfetct!");
+            if (ısSuccess || ısCompleted)
+            {
+                Console.WriteLine("Great!");
+            }
+            if (ısSuccess && !ısCompleted)
+            {
+                Console.WriteLine("Fine!");
+            }
+
+
+
+            int a = 3;
+            int b = 4;
+
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+
+            int sonuc1 = sayi1 / sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 ++;
+            Console.WriteLine(sonuc1);
+
+
+            int sonuc2 = 20 % 3;
+            Console.WriteLine(sonuc2);
+
+
+
+        }
+    }
+}
